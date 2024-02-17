3cc19d09bc5f47a04bee059602f3753c991e215f
MErlangga <muhammaderlanggaputraw@gmail.com>
2024-02-17 17:10:00 +0700

    app

--- config/app.php
+++ config/app.php
@@ -182,6 +182,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
